62629195a5f5c5e7902d7209228e504dc785654d
joaoBrRj91 <joao1991algoritmo@gmail.com>
2018-03-27 19:08:53 -0300

    Create struct for decision for open file or entry manually scrambled word and methods scopes for operations. This struct will have refactoring because this is  the fist iteration developement

--- WorldUnscrambler/Program.cs
+++ WorldUnscrambler/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace WorldUnscrambler
 {
@@ -10,6 +7,64 @@ namespace WorldUnscrambler
     {
         static void Main(string[] args)
         {
+
+            bool continueWordUnscramble = true;
+
+            do
+            {
+
+                  
+                Console.WriteLine("Please enter the option - F for file and M for Manual.");
+                var option = (Console.ReadLine() ?? string.Empty);
+
+                switch (option.ToUpper())
+                {
+                    case "F":
+                        Console.Write("Enter scrambled words file name: ");
+                        ExecuteScrambleWordsInFileScenario();
+                        break;
+
+                    case "M":
+                        Console.Write("Enter scrambled words manually: ");
+                        ExecuteScrambleWordsInManualEntryScenario();
+                        break;
+
+                    default:
+                        Console.WriteLine("Option was not recognized");
+                        break;
+                }
+
+
+                var continueWordUnscrambleDecision = string.Empty;
+
+                do
+                {
+
+                    Console.WriteLine("Do you want to continue? Y/N");
+                    continueWordUnscrambleDecision = (Console.ReadLine() ?? string.Empty);
+
+
+                } while       (!continueWordUnscrambleDecision.Equals("Y",StringComparison.OrdinalIgnoreCase) 
+                            && !continueWordUnscrambleDecision.Equals("N",StringComparison.OrdinalIgnoreCase));
+
+
+                continueWordUnscramble = (continueWordUnscrambleDecision.Equals("Y", StringComparison.OrdinalIgnoreCase));
+
+
+            } while (continueWordUnscramble);
+
         }
+
+
+        private static void ExecuteScrambleWordsInManualEntryScenario()
+        {
+
+        }
+
+        private static void ExecuteScrambleWordsInFileScenario()
+        {
+
+        }
+
     }
 }
